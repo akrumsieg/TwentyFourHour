f4c13e934753f81c2810fbeb05edf0f94e769c87
Alexandro Cazares <CazaresCode@gmail.com>
2021-06-01 18:04:35 -0400

    GET and POST for Post.

---
+++ TwentyFourHour.Models/PostModels/PostListItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwentyFourHour.Data;
+
+namespace TwentyFourHour.Models.PostModels
+{
+    public class PostListItem
+    {
+        public int PostId { get; set; }
+        public string PostTitle { get; set; }
+        public string PostText { get; set; }
+        public virtual List<Comment> ListOfComments { get; set; }
+        public virtual List<Like> ListOfLikes { get; set; }
+    }
+}
