1ac3a7679c550d168bdb5b5db76074a380c133ed
Aaron Krumsieg <a.krumsieg@gmail.com>
2021-06-01 16:01:58 -0500

    add Like entity

--- TwentyFourHour.Data/Like.cs
+++ TwentyFourHour.Data/Like.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,13 @@ namespace TwentyFourHour.Data
 {
     public class Like
     {
+        [Key]
+        public int LikeId { get; set; }
+
+        [ForeignKey(nameof(Post))]
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
+
+        public Guid OwnerId { get; set; }
     }
 }
