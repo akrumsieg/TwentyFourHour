3a74243a33d3631ea6ee3d0cb0b23c414f783d30
Marcia Rolfingsmeyer <rolfingsmeyer@gmail.com>
2021-06-01 17:41:46 -0400

    added create comment

--- TwentyFourHour.Data/Comment.cs
+++ TwentyFourHour.Data/Comment.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TwentyFourHour.Data
 {
-    class Comment
+    public class Comment
     {
+        [Key]
+        public int CommentId { get; set; }
+        public string CommentText { get; set; }
+       
+        [Required]
+        public Guid AuthorId { get; set; }
+
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+        
+        public virtual ICollection<Reply> Replies { get; set; }
+
+        
+        [ForeignKey(nameof(Post))]
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
+
+
     }
 }
