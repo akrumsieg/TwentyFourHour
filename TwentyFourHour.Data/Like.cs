aad37a9593dfdbbb275dd6f86bba801e50b61709
Aaron Krumsieg <a.krumsieg@gmail.com>
2021-06-01 16:58:56 -0500

    Add LikeCreate, LikeController, LikeService - Add Create methods

--- TwentyFourHour.Data/Like.cs
+++ TwentyFourHour.Data/Like.cs
@@ -17,6 +17,7 @@ namespace TwentyFourHour.Data
         public int PostId { get; set; }
         public virtual Post Post { get; set; }
 
+        [Required]
         public Guid OwnerId { get; set; }
     }
 }
---
+++ TwentyFourHour.Services/LikeService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwentyFourHour.Data;
+using TwentyFourHour.Models.LikeModels;
+
+namespace TwentyFourHour.Services
+{
+    public class LikeService
+    {
+        //field
+        private readonly Guid _userId;
+
+        //constructor
+        public LikeService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        //methods
+        //create
+        public bool CreateLike(LikeCreate model)
+        {
+            var entity = new Like()
+            {
+                PostId = model.PostId
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Likes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+
+    }
+}
