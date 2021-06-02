a776784857fdc4a581db17e3d5c016795e50ea66
Alexandro Cazares <CazaresCode@gmail.com>
2021-06-01 20:20:39 -0400

    Had to redo post entity. Big booboo.

---
+++ TwentyFourHour.WebAPI/Controllers/PostController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TwentyFourHour.Models.PostModels;
+using TwentyFourHour.Services;
+
+namespace TwentyFourHour.WebAPI.Controllers
+{
+    public class PostController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            var p = CreatePostService();
+            var posts = p.GetPosts();
+            return Ok(posts);
+        }
+
+        public IHttpActionResult Post(PostCreate post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePostService();
+
+            if (!service.CreatePost(post))
+                return InternalServerError();
+
+            return Ok(post);
+        }
+
+        private PostService CreatePostService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var postService = new PostService(userId);
+            return postService;
+        }
+    }
+}
