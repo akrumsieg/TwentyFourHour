c5e6ef7f9c3e8c1232f80c168f11488451936ac0
Alexandro Cazares <CazaresCode@gmail.com>
2021-06-01 21:25:33 -0400

    Added Display attribute in PostListItem.

--- TwentyFourHour.Models/PostModels/PostListItem.cs
+++ TwentyFourHour.Models/PostModels/PostListItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,13 @@ namespace TwentyFourHour.Models.PostModels
     public class PostListItem
     {
         public int PostId { get; set; }
+        
+        [Display(Name ="Title")]
         public string PostTitle { get; set; }
+
+        [Display(Name ="Text")]
         public string PostText { get; set; }
+        
         public virtual List<Comment> ListOfComments { get; set; }
         public virtual List<Like> ListOfLikes { get; set; }
     }
